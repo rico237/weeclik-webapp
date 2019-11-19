5d8b455c1542ab74dec1e23baaa7b55c24982f34
Grace <iris.b1991@gmail.com>
2019-11-19 21:57:40 +0100

    Correction Bug du à la tentative d'utilisation de redux-geoloc

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,13 +3,11 @@ import { combineReducers } from 'redux';
 import { alert } from './alert.reducer';
 import { registration } from './registration.reducer';
 import { authentication } from './authentication.reducer';
-import { reducer as geolocation } from 'react-redux-geolocation';
 
 const rootReducer = combineReducers({
     alert,
     registration,
-    authentication,
-    geolocation
+    authentication
 });
 
 export default rootReducer;
