44c8d6a920226d57e0c7575f94e422c9248f141d
Grace <iris.b1991@gmail.com>
2019-11-26 09:35:18 +0100

    Correction Bug sur les fichiers manquants

--- src/components/sessionPage/CommercesPage.js
+++ src/components/sessionPage/CommercesPage.js
@@ -11,25 +11,25 @@ import {
     CardContent,
 } from '@material-ui/core';
 import IMG1 from '../../assets/images/img1.png';
-import Artisanat from '../../assets/images/categories/cover1.jpg';
-import BienEtre from '../../assets/images/categories/cover2.jpg';
-import Decoration from '../../assets/images/categories/cover3.jpg';
-import ECommerce from '../../assets/images/categories/cover4.jpg';
-// import IMG1 from '../../assets/images/categories/cover5.jpg';
-import Hotellerie from '../../assets/images/categories/cover6.jpg';
-import Immobilier from '../../assets/images/categories/cover7.jpg';
-import Informatique from '../../assets/images/categories/cover8.jpg';
-import Alimentaire from '../../assets/images/categories/cover9.jpg';
-// import IMG1 from '../../assets/images/categories/cover10.jpg';
-// import IMG1 from '../../assets/images/categories/cover11.jpg';
-import Nautisme from '../../assets/images/categories/cover12.png';
-import Sante from '../../assets/images/categories/cover13.jpg';
-import Restauration from '../../assets/images/categories/cover14.jpg';
-// import IMG1 from '../../assets/images/categories/cover15.jpg';
-import Textile from '../../assets/images/categories/cover16.jpg';
-import Tourisme from '../../assets/images/categories/cover17.jpg';
-import Transport from '../../assets/images/categories/cover18.jpg';
-import Humanitaire from '../../assets/images/categories/cover19.jpg';
+// import Artisanat from '../../assets/images/categories/cover1.jpg';
+// import BienEtre from '../../assets/images/categories/cover2.jpg';
+// import Decoration from '../../assets/images/categories/cover3.jpg';
+// import ECommerce from '../../assets/images/categories/cover4.jpg';
+// // import IMG1 from '../../assets/images/categories/cover5.jpg';
+// import Hotellerie from '../../assets/images/categories/cover6.jpg';
+// import Immobilier from '../../assets/images/categories/cover7.jpg';
+// import Informatique from '../../assets/images/categories/cover8.jpg';
+// import Alimentaire from '../../assets/images/categories/cover9.jpg';
+// // import IMG1 from '../../assets/images/categories/cover10.jpg';
+// // import IMG1 from '../../assets/images/categories/cover11.jpg';
+// import Nautisme from '../../assets/images/categories/cover12.png';
+// import Sante from '../../assets/images/categories/cover13.jpg';
+// import Restauration from '../../assets/images/categories/cover14.jpg';
+// // import IMG1 from '../../assets/images/categories/cover15.jpg';
+// import Textile from '../../assets/images/categories/cover16.jpg';
+// import Tourisme from '../../assets/images/categories/cover17.jpg';
+// import Transport from '../../assets/images/categories/cover18.jpg';
+// import Humanitaire from '../../assets/images/categories/cover19.jpg';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { userActions } from '../../redux/actions';
@@ -135,7 +135,6 @@ class CommercesPage extends Component {
             .then(snapshot => {
                 snapshot.forEach((elt) => {
                     var _status;
-                    var _img;
 
                     switch (elt.get("statutCommerce")) {
                         case 0:
@@ -159,81 +158,11 @@ class CommercesPage extends Component {
                             break;
                     }
 
-                    switch (elt.get("typeCommerce")) {
-                        case "Alimentaire":
-                            _img = Alimentaire
-                            break;
-                        case "Artisanat":
-                            _img = Artisanat
-                            break;
-                        case "Bâtiment":
-                            _img = "Hors ligne - paiement annulé"
-                            break;
-                        case "Bien-être":
-                            _img = BienEtre
-                            break;
-                        case "Décoration":
-                            _img = Decoration
-                            break;
-                        case "Dépannage":
-                            _img = "Hors ligne - en attente de paiement"
-                            break;
-                        case "Evènement":
-                            _img = "Hors ligne - en attente de paiement"
-                            break;
-                        case "E-commerce":
-                            _img = ECommerce
-                            break;
-                        case "Fabricant":
-                            _img = "Hors ligne - en attente de paiement"
-                            break;
-                        case "Garagiste":
-                            _img = "Hors ligne - en attente de paiement"
-                            break;
-                        case "Hôtellerie":
-                            _img = Hotellerie
-                            break;
-                        case "Humanitaire":
-                            _img = Humanitaire
-                            break;
-                        case "Immobilier":
-                            _img = Immobilier
-                            break;
-                        case "Informatique":
-                            _img = Informatique
-                            break;
-                        case "Nautisme":
-                            _img = Nautisme
-                            break;
-                        case "Restauration":
-                            _img = Restauration
-                            break;
-                        case "Textile":
-                            _img = Textile
-                            break;
-                        case "Transport":
-                            _img = Transport
-                            break;
-                        case "Tourisme":
-                            _img = Tourisme
-                            break;
-                        case "Santé":
-                            _img = Sante
-                            break;
-                        case "Autre":
-                            _img = "Hors ligne - en attente de paiement"
-                            break;
-                    
-                        default:
-                            _img = IMG1
-                            break;
-                    }
-
                     newCommerces.push({
                         "id": elt.id,
                         "name": elt.get("nomCommerce"),
                         "status": _status,
-                        "imgCategory": _img,
+                        "imgCategory": IMG1,
                         "description": elt.get("description"),
                         "nbPartage": elt.get("nombrePartages")
                     });
