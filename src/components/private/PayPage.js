fadb43d52d752b985618cc18ce8da6d859039d7c
Rico237 <wolbereric@gmail.com>
2020-11-20 01:57:42 +0100

    Try prefill stripe checkout

--- src/components/private/PayPage.js
+++ src/components/private/PayPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Parse from 'parse';
 import { loadStripe } from '@stripe/stripe-js';
 import { Container, Grid, Typography, Avatar, Button, Box } from '@material-ui/core';
 import { createMuiTheme } from '@material-ui/core/styles';
@@ -30,6 +31,27 @@ class PayPage extends Component {
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            user: {
+                username: ''
+            }
+        };
+    }
+
+    componentDidMount() {
+        var currentUser = Parse.User.current();
+        if (currentUser) {
+            currentUser.fetch().then((snapshot) => {
+                var username = snapshot.getUsername();
+
+                this.setState(prevState => ({
+                    user: {
+                        ...prevState.user,
+                        username: username
+                    }
+                }))
+            })
+        }
     }
 
     async handleClick(event) {
@@ -48,6 +70,7 @@ class PayPage extends Component {
         // When the customer clicks on the button, redirect them to Checkout.
         const result = await stripe.redirectToCheckout({
           sessionId: session.id,
+          customerEmail: this.state.user.username
         });
     
         if (result.error) {
